3378e94d8b8295ab0872895e51a071d945f6898e
Kykeon <paulinoandresmiguel@gmail.com>
2023-02-25 12:21:06 -0500

    Add get_background and draw functions

--- tutorial.py
+++ tutorial.py
@@ -9,10 +9,8 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-# BACKGROUND
-BG_COLOR = (255, 255, 255)
-
 # CONSTANTS
+BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
@@ -20,11 +18,29 @@ PLAYER_VEL = 5
 # WINDOW
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# BACKGROUND
+def get_background(name):
+    image =  pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+    
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+# DRAW
+def draw(window, background, bg_image):
+    for tile in background: 
+        window.blit(bg_image, tile)
+
 # MAIN GAME LOOP
 def main(window):
-    pass
     run = True
     clock = pygame.time.Clock()
+    bacground, bg_image = get_background("Blue.png")
 
     while run:
         clock.tick(FPS)
