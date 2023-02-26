000ddcda997def2bba9b0411b303ca727f202aea
Kykeon <paulinoandresmiguel@gmail.com>
2023-02-25 21:34:12 -0500

    Add collision function. Add hit ahead and landed functions on player class

--- tutorial.py
+++ tutorial.py
@@ -100,12 +100,22 @@ class Player(pygame.sprite.Sprite):
             
     # Player loop
     def loop(self, fps):
-        # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
         
         self.fall_count += 1
         self.update_sprite()
         self.update()
+        
+    # Player collision
+    def landed(self):
+        self.fall_count = 0
+        self.y_vel = 0
+        self.jump_count = 0
+        
+    def hit_head(self):
+        self.count = 0
+        self.y_vel *= -1
         
     # Animate sprites
     def update_sprite(self):
@@ -160,8 +170,25 @@ def get_background(name):
 
     return tiles, image
 
+# Handle collisions
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects: 
+        if pygame.sprite.collide_mask(player, obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head()
+                
+        collided_objects.append(obj)
+    
+    return collided_objects
+        
+
 # HANDLE MOVE PLAYER
-def handle_move_player(player):
+def handle_move_player(player, objects):
     keys = pygame.key.get_pressed()
     
     player.x_vel = 0
