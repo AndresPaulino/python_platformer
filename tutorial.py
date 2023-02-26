50908fbeae3d982c9954577e45fe5dbd10affcab
Kykeon <paulinoandresmiguel@gmail.com>
2023-02-25 21:12:02 -0500

    create update animation sprites and update self in player class

--- tutorial.py
+++ tutorial.py
@@ -57,6 +57,7 @@ class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
     SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+    ANIMATION_DELAY = 4
     
     def __init__(self, x, y, width, height):
         # Set player rect to the given x, y, width, and height
@@ -86,14 +87,32 @@ class Player(pygame.sprite.Sprite):
             self.direction = "right"
             self.animation_count = 0
             
+    # Player loop
     def loop(self, fps):
         # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
         
         self.fall_count += 1
+        self.update_sprite()
+        self.update()
+        
+    # Animate sprites
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+            
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = self.animation_count // self.ANIMATION_DELAY % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+    
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
         
     def draw(self, window):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 # BACKGROUND
